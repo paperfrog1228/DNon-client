6855b9e3582dae8b5d574d27e03d9dae3658c9ce
paperfrog1228 <paperfrog@naver.com>
2020-09-25 23:43:00 +0900

    Test Jenkins auto build
    https://forum.unity.com/threads/cant-get-license-in-headless-nographics-mode-timeout-error.479960/?_ga=2.95061979.690129190.1602081284-1720971698.1600958134
    이거 시도해보기 전까지 한 일주일 낭비한 듯

---
+++ Unity_Dnon/Assets/Editor/AutoBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class AutoBuilder : MonoBehaviour
+{
+	[MenuItem("Build/Build WebGL")]
+	static void PerformBuild()
+	{
+		var sceneSettings = EditorBuildSettings.scenes;
+		string[] scenePaths = new string[sceneSettings.Length];
+
+		for (int i = 0; i < scenePaths.Length; ++i)
+		{
+			scenePaths[i] = sceneSettings[i].path;
+		}
+		BuildPipeline.BuildPlayer(scenePaths, "build/webgl", BuildTarget.WebGL, BuildOptions.Development);
+	}
+}
