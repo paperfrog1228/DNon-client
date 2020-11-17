18ce9a024838ea1a7ce8a8d7ceb2521d71f573eb
paperfrog <paperfrog@naver.com>
2020-11-17 18:24:13 +0900

    Add Sprite asset and chemical man
    에셋 구매한 것 포함하고 화학공학과맨 만들었다. 또 언제 애니메이션 구해오냐ㅠㅠ

--- Assets/Scripts/Object/Player.cs
+++ Assets/Scripts/Object/Player.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
 /// Player -> 조종하는 사람
 /// </summary>
@@ -9,6 +11,11 @@ public class Player : User
     private static Player instance = null;
     [SerializeField,Range(0,30)]
     private float speed=5.0f;
+    private int hp;
+    public Text hpText;
+    [ShowInInspector]public int Hp { get { return hp; } set { hp = value;
+            hpText.text = "Hp : " + hp;
+        } }
     private void Awake()
     {
         instance = this;
@@ -23,9 +30,23 @@ public class Player : User
         float vertical = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontal, Space.World);
         transform.Translate(Vector3.up  * Time.deltaTime * speed * vertical, Space.World);
+        if (Input.GetMouseButtonUp(0)) {
+            Attack(Input.mousePosition);
+        }
         SetNicknamePosition();
     }
-
+    private void Attack(Vector3 mousePos) {
+        var flask = Instantiate(Resources.Load("Prefab/flask")) as GameObject;
+        flask.transform.position = gameObject.transform.position;
+        flask.GetComponent<Flask>().targetPos=Camera.main.ScreenToWorldPoint(mousePos + new Vector3(0, 0, 10));
+        flask.SetActive(true);
+    }
+    #region Damage
+    public void ReceiveDamage(int damage) {
+        Hp -= damage;
+        Debug.Log(Time.time);
+    }
+    #endregion
     public static Player Instance()
     {
         return instance;
